20c9a51c9a056ec5616b0920f1a3cd13e4e4dec2
Devika0797 <devika-97@mail.ru>
2021-05-16 18:52:20 +0300

    lesson6

--- lessons/src/main/java/homework1/RunableJumpable.java
+++
@@ -1,9 +0,0 @@
-package homework1;
-
-public interface RunableJumpable {
-    int getMaxLength();
-    void run();
-
-    int getMaxHeight();
-    void jump();
-}
---
+++ lessons/src/main/java/lesson6/EchoConstants.java
@@ -0,0 +1,9 @@
+package lesson6;
+
+public class EchoConstants {
+
+    public final static String STOP_WORD = "/end";
+    public static final String HOST = "localHost";
+    public static final int PORT = 8189;
+
+}
