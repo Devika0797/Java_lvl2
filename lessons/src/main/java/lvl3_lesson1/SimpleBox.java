7cba09cc0e13c98d2f58992dda38064fd13df415
Devika0797 <devika-97@mail.ru>
2021-06-04 16:24:28 +0300

    level 3 lesson 1

---
+++ lessons/src/main/java/lvl3_lesson1/SimpleBox.java
@@ -0,0 +1,17 @@
+package lvl3_lesson1;
+
+//Требуется контейнер для хоранения объектов разного типа
+
+//POJO - Plain Old Java Object
+public class SimpleBox {
+        private final Object object;
+
+        public SimpleBox(Object object) {
+            this.object = object;
+        }
+
+        public Object getObject() {
+            return object;
+        }
+
+}
